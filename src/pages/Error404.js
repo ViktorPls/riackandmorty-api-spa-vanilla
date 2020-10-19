389921212d185cee7a4c37e5f2aea6f168686613
ViktorPls <gabriel.vides96@gmail.com>
2020-10-19 17:25:28 -0300

    Added page Error 404

---
+++ src/pages/Error404.js
@@ -0,0 +1,10 @@
+const Error404 = () => {
+    const view = `
+    <div class="Error404">
+        <h2>Error 404</h2>
+        <p>En ninguna parte del universo se encuentra lo que estas buscando terricola!</p>
+    </div>
+    `
+}
+
+export default Error404
